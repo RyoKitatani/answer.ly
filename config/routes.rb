78e86a85594ec3a09d42efe746a8b18c97410923
RyoKitatani <ryo0929m@gmail.com>
2021-04-04 07:41:04 +0000

    devise基本ファイル完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+
+  devise_for :admins, controllers: {
+    sessions:  'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
+  }
+
+  devise_for :members, controllers: {
+     sessions:     'members/sessions',
+     passwords:    'members/passwords',
+     registrations: 'members/registrations'
+  }
+
 end
