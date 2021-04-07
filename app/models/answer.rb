e46c782c2a2c5689178563aa81836fe8715b83c0
RyoKitatani <ryo0929m@gmail.com>
2021-04-07 07:31:13 +0000

    answer.rbへのアソシエーション追記

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,5 +3,5 @@ class Answer < ApplicationRecord
   belongs_to :member
   belongs_to :question
   has_many   :answer_likes, dependent: :destroy
-
+  has_many   :responses, dependent: :destroy
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,9 @@
 class Question < ApplicationRecord
-
+  
   has_many   :question_tags, dependent: :destroy
   has_many   :tags, through: :question_tags, dependent: :destroy
   has_many   :question_likes, dependent: :destroy
   has_many   :answers, dependent: :destroy
   belongs_to :member
-
+  
 end
