f9d53f66d49f0520634fbcdeb65bd925bd3e6379
RyoKitatani <ryo0929m@gmail.com>
2021-04-10 14:24:32 +0000

    topページのレイアウト修正

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -3,6 +3,7 @@ class Public::QuestionsController < ApplicationController
   before_action :authenticate_member!, except:[:show]
 
   def show
+    @members = Member.all
     @member = current_member
     @question = Question.find(params[:id])
     @tags = Tag.all.order(created_at: :desc)
--- app/controllers/public/responses_controller.rb
+++ app/controllers/public/responses_controller.rb
@@ -7,7 +7,8 @@ class Public::ResponsesController < ApplicationController
     @response.member_id = current_member.id
     if @response.save
       flash[:notice] = "You have commented successfully"
-      redirect_to request.referer
+      # redirect_to request.referer
+      render :create
     else
       @question = Question.find(params[:id])
       @tags = Tag.all.order(created_at: :desc)
@@ -19,7 +20,8 @@ class Public::ResponsesController < ApplicationController
     @answer = Answer.find(params[:answer_id])
     @response = Response.find_by(id: params[:id], answer_id: params[:answer_id])
     @response.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
+    render :destroy
   end
 
 
