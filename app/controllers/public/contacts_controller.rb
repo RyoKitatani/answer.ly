f8903525dee04c68981ca3b9b3abd23bd20b9475
RyoKitatani <ryo0929m@gmail.com>
2021-04-14 06:14:45 +0000

    contactフォーム&機能実装

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,4 +1,25 @@
 class Public::ContactsController < ApplicationController
   def new
+    @contact = Contact.new
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+       ContactMailer.contact_mail(@contact).deliver
+       flash[:success] = "お問い合わせが完了しました。"
+       redirect_to request.referer
+    else
+      flash[:danger] = "お問い合わせが上手くできませんでした。もう一度お試しください。"
+      render new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :email, :title, :message)
+
+  end
+
 end
