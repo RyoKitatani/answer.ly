ba747f88ac575ae3b47ae426bada15a1de3fd026
RyoKitatani <ryo0929m@gmail.com>
2021-04-14 03:13:13 +0000

    topページjquery追加

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,3 +19,33 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(window).on('load',function(){
+// ここから文字を<span></span>で囲む記述
+$('.typ').children().andSelf().contents().each(function() {
+if (this.nodeType == 3) {
+$(this).replaceWith($(this).text().replace(/(\S)/g, '<span>$1</span>'));
+}
+});
+// ここから一文字ずつフェードインさせる記述
+$('.typ').css({'opacity':1});
+for (var i = 0; i <= $('.typ').children().size(); i++) {
+$('.typ').children('span:eq('+i+')').delay(100*i).animate({'opacity':1},50);
+};
+});
+
+$(function () {
+  $("textarea").keyup(function(){
+    var counter = $(this).val().length;
+    $("#countUp").text(counter);
+
+    if(counter == 0){
+      $("#countUp").text("0");
+    }
+    if(counter >= 10){
+      $("#countUp").css("color","red");
+    } else {
+      $("#countUp").css("color","#666");
+    }
+  });
+});
