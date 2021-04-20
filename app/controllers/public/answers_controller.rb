21df63c2b99c4e975c4abb0257c57373a78c1202
RyoKitatani <ryo0929m@gmail.com>
2021-04-20 15:01:18 +0000

    回答createの修正

--- app/controllers/public/answers_controller.rb
+++ app/controllers/public/answers_controller.rb
@@ -1,9 +1,5 @@
 class Public::AnswersController < ApplicationController
-
-  def edit
-    # @question = Question.find(params[:question_id])
-    # @answer = Answer.find(params[:id])
-  end
+  before_action :member_header_actions
 
   def create
     @question = Question.find(params[:question_id])
@@ -13,11 +9,12 @@ class Public::AnswersController < ApplicationController
     if @answer.save
       flash[:success] = "回答しました。"
       redirect_to request.referer
-      # render :create
     else
-      @question = Question.find(params[:id])
+      @question = Question.find(params[:question_id])
       @tags = Tag.all.order(created_at: :desc)
-      render "questions/show"
+      @response = Response.new
+      flash[:danger] = "回答を入力してください"
+      redirect_to question_path(@question)
     end
   end
 
@@ -27,7 +24,6 @@ class Public::AnswersController < ApplicationController
     @answer.destroy
     flash[:success] = "回答を削除しました。"
     redirect_to request.referer
-    # render :destroy
   end
 
   def bestanswer
