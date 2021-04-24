af4f6c3c8694f62f46323948c49a14986aea859e
RyoKitatani <ryo0929m@gmail.com>
2021-04-24 10:04:08 +0000

    db/referencesのdafalutのみ削除

--- db/migrate/20210404075128_create_question_tags.rb
+++ db/migrate/20210404075128_create_question_tags.rb
@@ -1,8 +1,8 @@
 class CreateQuestionTags < ActiveRecord::Migration[5.2]
   def change
     create_table :question_tags do |t|
-      t.references :question, foreign_key: true, null: false, default: ""
-      t.references :tag, foreign_key: true, null: false, default: ""
+      t.references :question, foreign_key: true, null: false
+      t.references :tag, foreign_key: true, null: false
 
       t.timestamps
     end
