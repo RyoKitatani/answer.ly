6f0a97cca095cb75bc6d63c2f967a79bb1697018
RyoKitatani <ryo0929m@gmail.com>
2021-04-24 09:35:07 +0000

    db/FK設定修正

--- db/migrate/20210404075028_create_questions.rb
+++ db/migrate/20210404075028_create_questions.rb
@@ -1,7 +1,7 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
   def change
     create_table :questions do |t|
-      t.references :member, foreign_key: true, null: false, default: ""
+      t.references :member, foreign_key: true, default: ""
       t.string  :title, null: false, default: ""
       t.text    :content, null: false, default: ""
       t.integer  :best_answer_id, default: ""
