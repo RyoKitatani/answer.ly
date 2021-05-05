6b8bdc986d8757603aea195bf02323bd3b564761
RyoKitatani <ryo0929m@gmail.com>
2021-05-05 15:23:28 +0000

    relationshipコントローラーへ追記

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -2,6 +2,7 @@ class Public::RelationshipsController < ApplicationController
   before_action :authenticate_member!
 
   def create
+    @member = Member.find(params[:relationship][:following_id])
     current_member.follow(params[:id])
     @member.create_notification_follow!(current_member)
     redirect_to request.referer
