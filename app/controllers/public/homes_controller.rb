86dc0572c60a99d8ff363bd76e950155f9b09803
RyoKitatani <ryo0929m@gmail.com>
2021-05-17 03:30:52 +0000

    homes/top/タグ一覧とランキングのN+1対応

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -6,9 +6,9 @@ class Public::HomesController < ApplicationController
     @questions = Question.all.page(params[:page]).reverse_order.per(10)
     @answers = Answer.all
     @members = Member.all
-    @tags = Tag.all.page(params[:page]).order_by_questions.per(5)
-    @member_all = Member.all.page(params[:page]).order_by_answers.per(5)
-    @member_alls = Member.all.page(params[:page]).order_by_question.per(5)
+    @tags = Tag.includes(:questions).page(params[:page]).order_by_questions.per(5)
+    @member_all = Member.includes(:answers).page(params[:page]).order_by_answers.per(5)
+    @member_alls = Member.includes(:questions).page(params[:page]).order_by_question.per(5)
   end
 
   def about
