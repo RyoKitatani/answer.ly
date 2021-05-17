e8e231e5a8454395acf7dc965e082ae8e71b5542
RyoKitatani <ryo0929m@gmail.com>
2021-05-17 06:36:30 +0000

    gem/bullet導入及びN＋1一部対応

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,13 +2,12 @@ class Public::HomesController < ApplicationController
   layout "homes"
 
   def top
-    @question_all = Question.all.order(created_at: :desc)
-    @questions = Question.all.page(params[:page]).reverse_order.per(10)
-    @answers = Answer.all
-    @members = Member.all
-    @tags = Tag.includes(:questions).page(params[:page]).order_by_questions.per(5)
-    @member_all = Member.includes(:answers).page(params[:page]).order_by_answers.per(5)
-    @member_alls = Member.includes(:questions).page(params[:page]).order_by_question.per(5)
+    @question_all = Question.includes([:member], [:tags], [:question_tags], [:answers]).order(created_at: :desc)
+    @questions = Question.includes([:member], [:tags], [:question_tags], [:answers]).page(params[:page]).reverse_order.per(10)
+    @members = Member.includes([:questions])
+    @tags = Tag.includes([:questions]).page(params[:page]).order_by_questions.per(5)
+    @member_all = Member.includes([:answers]).page(params[:page]).order_by_answers.per(5)
+    @member_alls = Member.includes([:questions]).page(params[:page]).order_by_question.per(5)
   end
 
   def about
